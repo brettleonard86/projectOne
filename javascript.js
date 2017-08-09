81150041d9a12205f9ecee4c7bf921884bd2a76f
JohnMichaelKU <Johnmichaelmcgrew@gmail.com>
2017-08-08 19:36:29 -0500

    hey

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 $(document).on('click', '#submit', function() {
-  var queryURL = "http://andruxnet-random-famous-quotes.p.mashape.com/"
+  var queryURL = 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies&count=1'
   $.ajax({
-    url: gueryURL,
-    method: 'GET',
+  type: "GET",
+  beforeSend: function(request) {
+    request.setRequestHeader("X-Mashape-Key", "0d5VBPMK1qmshZ6l7Tr5SY6iUevop1SlBZijsnZMaVzStdHLhO");
+  },
+  url: queryURL,
   }).done(function(response) {
     console.log(response)
   })
